db5616e712d71c56eeeba13e94782c4fd4a293f7
therealpaulgg <paul@paul.systems>
2023-11-10 02:50:57 -0700

    add API key

--- pkg/web/router/main.go
+++ pkg/web/router/main.go
@@ -2,19 +2,35 @@ package router
 
 import (
 	"fmt"
+	"os"
 
 	"net/http"
 
 	"github.com/go-chi/chi"
 
+	"github.com/rs/zerolog/log"
 	"github.com/samber/do"
-	"github.com/therealpaulgg/ssh-sync-server/pkg/web/middleware"
 	"github.com/therealpaulgg/recipe-book-proxy/pkg/web/router/routes"
+	"github.com/therealpaulgg/ssh-sync-server/pkg/web/middleware"
 )
 
 func Router(i *do.Injector) chi.Router {
 	baseRouter := chi.NewRouter()
 	baseRouter.Use(middleware.Log)
+	// Add simple auth middleware to 401 requests that do not include the API_KEY env var in Authorization as 'Bearer <key>'
+	if os.Getenv("API_KEY") != "" {
+		baseRouter.Use(func(h http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.Header.Get("Authorization") != fmt.Sprintf("Bearer %s", os.Getenv("API_KEY")) {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+				h.ServeHTTP(w, r)
+			})
+		})
+	} else {
+		log.Warn().Msg("API_KEY not set, no auth will be used")
+	}
 
 	apiV1Router := chi.NewRouter()
 	apiV1Router.Get("/", func(w http.ResponseWriter, r *http.Request) {
